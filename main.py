facad51ccbfff7fcf5e4b112fd30602e991ac2eb
Adam HE <adam.he.future@gmail.com>
2023-10-07 16:09:25 +0100

    add melody & rhythm fitness fn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import math
+
 from music21 import converter, stream, note, chord, meter, tempo, key, instrument
 import random
 import numpy as np
+
 
 def load_midi(file_path):
     # file_path = "Themes/Twinkle-Little-Star (Long Version).mid"
@@ -11,30 +14,33 @@ def load_midi(file_path):
         # if isinstance(element, stream.Measure):
         #         melody.append(element)
         if isinstance(element, stream.Voice):
-            voice_number+=1
+            voice_number += 1
         if voice_number % 2 == 0:
             if isinstance(element, note.Note):
-                melody.append([element.pitch.midi, element.beat, element.offset, element.duration.quarterLength, element.volume.velocity])
+                melody.append([element.pitch.midi, element.beat, element.offset, element.duration.quarterLength,
+                               element.volume.velocity])
             elif isinstance(element, chord.Chord):
                 # Extract only the leading (higher pitch) note from the chord
                 leading_note = max(element.pitches, key=lambda x: x.midi)
-                melody.append([leading_note.midi, element.beat, element.offset, element.duration.quarterLength, element.volume.velocity])
+                melody.append([leading_note.midi, element.beat, element.offset, element.duration.quarterLength,
+                               element.volume.velocity])
     return melody
+
 
 def create_midi_file(melody, output_file_path, bpm=120):
     # Create a stream for the melody
     # melody_stream = stream.Score()
     melody_part = stream.Part()
     # melody_stream.append(melody_part)
-    
+
     # Add notes to the melody part
     for note_value in melody:
-        pitch, beat, offset, duration,  velocity = note_value
+        pitch, beat, offset, duration, velocity = note_value
         n = note.Note(pitch, quarterLength=duration)
         n.offset = offset
         n.volume.velocity = int(velocity * 127)  # Velocity range: 0 to 127
         melody_part.append(n)
-    
+
     # Set tempo and time signature
     melody_part.append(tempo.MetronomeMark(number=bpm))
     melody_part.append(meter.TimeSignature('4/4'))
@@ -45,25 +51,29 @@ def create_midi_file(melody, output_file_path, bpm=120):
 
     # Write the MIDI file
     melody_part = stream.Score([melody_part])
-    
+
     # Write the melody to the MIDI file
     melody_part.write('midi', fp=output_file_path)
     print(f"Melody saved to {output_file_path}")
 
+
 def split_note(note_value, n_splits=2):
     # Split note into n notes with half duration
     splits = []
-    new_duration = note_value[3]/n_splits
+    new_duration = note_value[3] / n_splits
     for i in range(n_splits):
-        note = [note_value[0], note_value[1] + i * new_duration, note_value[2] + i * new_duration, new_duration, note_value[4]]
+        note = [note_value[0], note_value[1] + i * new_duration, note_value[2] + i * new_duration, new_duration,
+                note_value[4]]
         splits.append(note)
     return splits
+
 
 def swap_notes(melody):
     # Randomly choose two indices and swap the notes
     idx1, idx2 = random.sample(range(len(melody)), 2)
     melody[idx1][0], melody[idx2][0] = melody[idx2][0], melody[idx1][0]
     return melody
+
 
 def add_note_sequence(note_value):
     # Create a chord degree note sequence (for simplicity, using a major chord)
@@ -72,6 +82,7 @@ def add_note_sequence(note_value):
     for i in range(len(sequence)):
         note_sequence[i][0] = sequence[i]
     return note_sequence
+
 
 def initialize_population(melody, population_size):
     population = [melody.copy()]  # Start with the original melody
@@ -86,7 +97,7 @@ def initialize_population(melody, population_size):
             # note_idx = choice([i for i in range(0, len(variation)-1) if not i in measure_indices])
             note_idx = random.randint(0, len(variation) - 1)
             # variation.pop(note_idx)
-            variation[note_idx: note_idx+1] = split_note(variation[note_idx])
+            variation[note_idx: note_idx + 1] = split_note(variation[note_idx])
             # variation.extend(split_note(variation[note_idx]))
         elif modification_type == 'swap' and len(variation) > 1:
             # Swap two random notes within the melody
@@ -94,12 +105,13 @@ def initialize_population(melody, population_size):
         elif modification_type == 'add':
             # Add note sequence by extending a random note
             note_idx = random.randint(0, len(variation) - 1)
-            variation[note_idx:note_idx+1] = add_note_sequence(variation[note_idx])
+            variation[note_idx:note_idx + 1] = add_note_sequence(variation[note_idx])
         population.append(variation)
     return population
 
+
 def crossover(parent1, parent2, crossover_rate):
-    if np.random.rand() < crossover_rate: 
+    if np.random.rand() < crossover_rate:
         # Single point crossover: Split parents at a random point and swap the segments
         crossover_point = random.randint(1, min(len(parent1), len(parent2)) - 1)
         child1 = parent1[:crossover_point] + parent2[crossover_point:]
@@ -110,7 +122,8 @@ def crossover(parent1, parent2, crossover_rate):
         child2 = parent2.copy()
         return child1, child2
 
-# Define duration mutation: A note is randomly selected and its duration is 
+
+# Define duration mutation: A note is randomly selected and its duration is
 # either doubled or reduced to half
 def duration_mutation(individual):
     note_idx = random.randint(0, len(individual) - 1)
@@ -121,20 +134,22 @@ def duration_mutation(individual):
     else:
         if np.random.rand() < 0.5:
             individual[note_idx][3] /= 2
-        else: 
+        else:
             individual[note_idx][3] *= 2
     return individual
 
-# Pitch Mutation: A note out of harmony will be selected and changed to 
+
+# Pitch Mutation: A note out of harmony will be selected and changed to
 # one of a harmony degree based on the previous note.
 def pitch_mutation(individual):
     note_idx = random.randint(1, len(individual) - 2)
     prev_note_idx = note_idx - 1
     previous_note = individual[prev_note_idx][0]
-    chord_notes_previous_bar = [previous_note, previous_note-7, 
+    chord_notes_previous_bar = [previous_note, previous_note - 7,
                                 previous_note + 7]
     individual[note_idx][0] = min(random.choice(chord_notes_previous_bar), 127)
     return individual
+
 
 # Define mutation operator (for example, random note change)
 def mutate(individual, mutation_rate):
@@ -148,12 +163,85 @@ def mutate(individual, mutation_rate):
                 individual = duration_mutation(individual)
     return individual
 
+
 # Define fitness function (example: harmonic, similarity, rhythmic diversity)
 def calculate_fitness(individual, original_melody):
     # Calculate fitness based on harmony, similarity, rhythmic diversity
-    # ...
+    similarity = melody_similarity(individual, original_melody)
+    complexity = tempo_complexity(individual)
+    print()
+    print('similairty:', similarity)
+    print('complexity:', complexity)
+
     fitness = 1  # Calculate the fitness score
     return fitness
+
+
+def chop_into_bars(melody):
+    chop_at = [idx for idx, note in enumerate(melody) if note[1] == 1] + [len(melody)]
+    chop_into = [melody[chop_at[i]:chop_at[i + 1]] for i in range(len(chop_at) - 1)]
+    return chop_into
+
+
+def melody_similarity(melody_a, melody_b):
+    melody_a = chop_into_bars(melody_a)
+    melody_b = chop_into_bars(melody_b)
+    assert len(melody_a) == len(melody_b), 'Two melodies should have the same length'
+    ce_differences = [central_effect(bar) - central_effect(melody_b[i]) for i, bar in enumerate(melody_a)]
+    ce_distances = [np.sum(np.square(diff)) for diff in ce_differences]
+    score = 100 - np.average(ce_distances)
+    return score
+
+
+def central_effect(notes):
+    positions = np.array([note_coordinate(n) for n in notes])
+    durations = [n[3] for n in notes]
+    weights = np.array([d / sum(durations) for d in durations])
+    weighted_sum = np.sum(positions * weights[:, np.newaxis], axis=0)
+    return weighted_sum
+
+
+def note_coordinate(note, h=math.sqrt(1 / 6)):
+    pitch_class_to_k = [0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5]
+    pitch_class = note[0] % 12
+    k = pitch_class_to_k[pitch_class]
+    t = k * math.pi / 2
+    x = math.sin(t)
+    y = math.cos(t)
+    z = k * h
+    return [x, y, z]
+
+
+def tempo_complexity(melody):
+    max_metricity = [5,
+                     5 + 4,
+                     5 + 4 + 3,
+                     5 + 4 + 3 * 2,
+                     5 + 4 + 3 * 2 + 2,
+                     5 + 4 + 3 * 2 + 2 * 2,
+                     5 + 4 + 3 * 2 + 2 * 3,
+                     5 + 4 + 3 * 2 + 2 * 4,
+                     5 + 4 + 3 * 2 + 2 * 4 + 1,
+                     5 + 4 + 3 * 2 + 2 * 4 + 2,
+                     5 + 4 + 3 * 2 + 2 * 4 + 3,
+                     5 + 4 + 3 * 2 + 2 * 4 + 4,
+                     5 + 4 + 3 * 2 + 2 * 4 + 5,
+                     5 + 4 + 3 * 2 + 2 * 4 + 6,
+                     5 + 4 + 3 * 2 + 2 * 4 + 7,
+                     5 + 4 + 3 * 2 + 2 * 4 + 8,
+                     ]
+    melody = chop_into_bars(melody)
+    complexity = [max_metricity[len(bar)-1] - metricity(bar) for bar in melody]
+    score = sum(complexity) / len(complexity)
+    return score
+
+
+def metricity(notes):
+    weights = [5, 1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1]
+    positions = [int(n[2] * 4) for n in notes]
+    metricity = sum([weights[p] for p in positions])
+    return metricity
+
 
 # Main genetic algorithm function
 def genetic_algorithm(original_melody, population_size, generations, crossover_rate, mutation_rate):
@@ -178,7 +266,6 @@ def genetic_algorithm(original_melody, population_size, generations, crossover_r
 
 # Example usage
 if __name__ == "__main__":
-
     # Load original melody from MIDI file
     original_melody = load_midi("Themes/twinkle-twinkle-little-star.mid")
     
